f409ffb68e9d1db37ac88ce068346774c5e692fe
rakesh93 <mk935511@gmail.com>
2019-12-08 05:44:02 +0530

    by rakesh second

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -7,6 +7,8 @@ public class AppiumTest {
 	public void NativeAPpAndroid()
 	{
 		System.out.println("NativeAPpAndroid");
+		System.out.println("NativeAPpAndroid2");
+		System.out.println("NativeAPpAndroid3");
 	}
 	
 	@Test
